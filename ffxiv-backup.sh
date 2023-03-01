1c721288bd8333d2324ed6397fe4c2afcb73d580
Lily Skye <git@suchipi.tv>
2023-03-01 16:44:01 -0700

    small tweaks

--- ffxiv-backup.sh
+++ ffxiv-backup.sh
@@ -8,27 +8,30 @@ TO_DIR=${2:-"./backup"}
 echo FROM_DIR: $FROM_DIR
 echo TO_DIR: $TO_DIR
 
-mkdir -p $TO_DIR
+mkdir -p "$TO_DIR"
 
-FROM_DIR_ABSOLUTE=$(realpath $FROM_DIR)
-TO_DIR_ABSOLUTE=$(realpath $TO_DIR)
+FROM_DIR_ABSOLUTE=$(realpath "$FROM_DIR")
+TO_DIR_ABSOLUTE=$(realpath "$TO_DIR")
 
-cd $FROM_DIR_ABSOLUTE
+cd "$FROM_DIR_ABSOLUTE"
 
-# if you don't use GShade, delete the relevant lines from below
-for path in \
-  ffxivConfig/FFXIV_CH* \
-  ffxivConfig/MACROSYS.dat \
-  ffxiv/game/dxgi.dll \
-  ffxiv/game/d3dcompiler_47.dll \
-  ffxiv/game/GShade.ini \
-  ffxiv/game/gshade-presets \
-  ffxiv/game/gshade-shaders \
-  ffxiv/game/notification.wav \
-; do
-  echo "$FROM_DIR/$path -> $TO_DIR/$path"
+FILES=(
+  ffxivConfig/FFXIV_CH*
+  ffxivConfig/MACROSYS.dat
+  ffxiv/game/dxgi.dll
+  ffxiv/game/d3dcompiler_47.dll
+  ffxiv/game/GShade.ini
+  ffxiv/game/gshade-presets/
+  ffxiv/game/gshade-shaders/
+  ffxiv/game/notification.wav
+)
 
+for path in "${FILES[@]}"; do
+  src="$FROM_DIR_ABSOLUTE/$path"
   dest="$TO_DIR_ABSOLUTE/$path"
-  mkdir -p $(dirname $dest)
-  cp --recursive --remove-destination $path $dest
+
+  echo "$src -> $dest"
+
+  mkdir -p $(dirname "$dest")
+  cp --recursive --remove-destination "$src" "$dest"
 done
